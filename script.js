0dc2e210d36f5d8b9fae64c2ce9e33a8b4ed6b13
nashie1004 <naocenar@gmail.com>
2023-02-20 10:21:06 +0800

    added more js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import {dfs} from './dfs.js'
+import {bfs} from './bfs.js'
+import {dijkstra} from './dijkstra.js'
+import {aStar} from './aStar.js'
+
 const gridContainer = document.querySelector('#container')
 const colCount = 47
 const rowCount = 21
@@ -11,14 +16,56 @@ let size = 30
 gridContainer.style.gridTemplateColumns = `repeat(${colCount}, ${size}px)`
 gridContainer.style.gridTemplateRows = `repeat(${rowCount}, ${size}px)`
 
-let count = 0
-for (let i = 0; i < rowCount; i++){
+let graph = {}
+let gridItemsArray = []
+
+function createDivs(){
+
+  let count = 0
+  for (let i = 0; i < rowCount; i++){
+    gridItemsArray[i] = []
     for (let j = 0; j < colCount; j++){
       let div = document.createElement('div')
       div.classList.add('grid-item')
       div.dataset.index = count
       gridContainer.append(div)
+      div.innerHTML = count
+  
+      gridItemsArray[i][j] = count
       count++
     }
+  }
+
 }
+createDivs()
+
+function neighbors(){
+  
+  for (let i = 0; i < gridItemsArray.length; i++){
+    for (let j = 0; j < gridItemsArray[i].length; j++){
+      
+      let top = gridItemsArray[i-1][j]
+      let bot = gridItemsArray[i+1][j]
+      let left = gridItemsArray[i][j-1]
+      let right = gridItemsArray[i][j+1]
+
+      graph[gridItemsArray[i][j]] = {neighbors: {top: top, bot: bot, left: left, right: right}, coordinate: {x: j, y: i}, state: 'empty' }
+      
+      
+    }
+  }
+
+}
+neighbors()
+console.log(gridItemsArray)
+console.log(graph)
+
+dfs()
+bfs()
+dijkstra()
+aStar()
+
+
+
+
   
